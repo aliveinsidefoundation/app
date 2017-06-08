d237ba36a21004d37e33688da9ed77a1b345d505
Olaolu <olaoluhimself@yahoo.com>
2017-06-08 13:14:18 -0500

    nothing

--- env/devel-config.js
+++ env/devel-config.js
@@ -1,7 +1,7 @@
 const KEYS = {
     SPOTIFY_CLIENT: 'b7e5e8676be84916b431c98d51b85d5c',
     SPOTIFY_TOKEN: '5176ca36c9964509a82916d65aefc719',
-    SPOTIFY_REDIRECT: 'http://localhost:8080/login'
+    SPOTIFY_REDIRECT: 'http://https://holaholu.github.io/AliveInsideFoundation//login'
 };
 
 export default KEYS;
